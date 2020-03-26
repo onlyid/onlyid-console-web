fc25a784a89de512c5ebd2bbcb7ce77927d0a5e8
ltb <liangtb@qq.com>
2020-03-26 13:29:54 +0800

    feat: 完成client的otp config

--- src/my/constants.js
+++ src/my/constants.js
@@ -22,3 +22,34 @@ export const CLIENT_TYPE_TEXT = {
     APP: "APP",
     OTHER: "其他"
 };
+
+export const OTP_TYPE_TEXT = {
+    NUMBER: "随机 数字（推荐）",
+    NUMBER_LETTER_LOWERCASE: "随机 数字 + 小写字母",
+    NUMBER_LETTER: "随机 数字 + 大小写字母"
+};
+
+export const OTP_LENGTH_TEXT = {
+    4: "4位",
+    6: "6位（推荐）",
+    8: "8位（安全）"
+};
+
+export const OTP_EXPIRE_TEXT = {
+    5: "5分钟（安全）",
+    10: "10分钟（推荐）",
+    20: "20分钟"
+};
+
+export const OTP_FAIL_TEXT = {
+    5: "5次（安全）",
+    10: "10次（推荐）",
+    20: "20次"
+};
+
+export const OTP_TEMPLATE_TEXT = {
+    383959: "APP的验证码为：Y，该验证码X分钟内有效，请勿泄露于他人",
+    383106: "(Y)APP验证码。工作人员不会向您索要，请勿向任何人泄露，以免造成账户或资金损失",
+    383105: "您的验证码是：Y。此验证码用于[APP]，有效期X分钟",
+    383087: "(Y)APP验证码，X分钟内有效"
+};
--- src/pages/AppManage/OtpConfig.js
+++ src/pages/AppManage/OtpConfig.js
@@ -1,9 +1,171 @@
 import React, { PureComponent } from "react";
+import http from "my/http";
+import { Button, Descriptions, Form, message, Radio, Select } from "antd";
+import { connect } from "react-redux";
+import { OTP_TYPE_TEXT } from "my/constants";
+import { OTP_LENGTH_TEXT } from "my/constants";
+import { OTP_EXPIRE_TEXT } from "my/constants";
+import { OTP_FAIL_TEXT } from "my/constants";
+import { OTP_TEMPLATE_TEXT } from "my/constants";
 
-class OtpConfig extends PureComponent {
+const { Item } = Descriptions;
+const { Option } = Select;
+
+class Edit extends PureComponent {
+    submit = () => {
+        const { form, onSave, info } = this.props;
+
+        form.validateFields(async (err, values) => {
+            if (err) return;
+
+            await http.put(`clients/${info.clientId}/otp-config`, values);
+
+            onSave();
+            message.success("保存成功");
+        });
+    };
+
     render() {
-        return <div>otp config</div>;
+        const {
+            info,
+            form: { getFieldDecorator },
+            onCancel
+        } = this.props;
+
+        return (
+            <Form layout="vertical">
+                <Form.Item label="验证码长度">
+                    {getFieldDecorator("length", {
+                        initialValue: String(info.length)
+                    })(
+                        <Radio.Group>
+                            {Object.keys(OTP_LENGTH_TEXT).map(key => (
+                                <Radio key={key} value={key}>
+                                    {OTP_LENGTH_TEXT[key]}
+                                </Radio>
+                            ))}
+                        </Radio.Group>
+                    )}
+                </Form.Item>
+                <Form.Item label="验证码类型">
+                    {getFieldDecorator("type", {
+                        initialValue: info.type
+                    })(
+                        <Radio.Group>
+                            {Object.keys(OTP_TYPE_TEXT).map(key => (
+                                <Radio key={key} value={key}>
+                                    {OTP_TYPE_TEXT[key]}
+                                </Radio>
+                            ))}
+                        </Radio.Group>
+                    )}
+                </Form.Item>
+                <Form.Item label="验证码有效期">
+                    {getFieldDecorator("expireMin", {
+                        initialValue: String(info.expireMin)
+                    })(
+                        <Radio.Group>
+                            {Object.keys(OTP_EXPIRE_TEXT).map(key => (
+                                <Radio key={key} value={key}>
+                                    {OTP_EXPIRE_TEXT[key]}
+                                </Radio>
+                            ))}
+                        </Radio.Group>
+                    )}
+                </Form.Item>
+                <Form.Item label="允许失败次数">
+                    {getFieldDecorator("maxFailCount", {
+                        initialValue: String(info.maxFailCount)
+                    })(
+                        <Radio.Group>
+                            {Object.keys(OTP_FAIL_TEXT).map(key => (
+                                <Radio key={key} value={key}>
+                                    {OTP_FAIL_TEXT[key]}
+                                </Radio>
+                            ))}
+                        </Radio.Group>
+                    )}
+                </Form.Item>
+                <Form.Item label="验证码模板" extra="APP是你的应用名，X是有效期，Y是验证码">
+                    {getFieldDecorator("template", {
+                        initialValue: String(info.template)
+                    })(
+                        <Select>
+                            {Object.keys(OTP_TEMPLATE_TEXT).map(key => (
+                                <Option key={key} value={key}>
+                                    {OTP_TEMPLATE_TEXT[key]}
+                                </Option>
+                            ))}
+                        </Select>
+                    )}
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" onClick={this.submit}>
+                        保存
+                    </Button>
+                    <Button onClick={onCancel} style={{ marginLeft: 20 }}>
+                        取消
+                    </Button>
+                </Form.Item>
+            </Form>
+        );
     }
 }
 
-export default OtpConfig;
+const EditForm = Form.create()(Edit);
+
+class OtpConfig extends PureComponent {
+    state = {
+        info: {},
+        isEdit: false
+    };
+
+    componentDidMount() {
+        this.initData();
+    }
+
+    initData = async () => {
+        const {
+            appManage: { selectedKey }
+        } = this.props;
+
+        const info = await http.get(`clients/${selectedKey}/otp-config`);
+        this.setState({ info });
+    };
+
+    showEdit = () => {
+        this.setState({ isEdit: true });
+    };
+
+    onSave = () => {
+        this.setState({ isEdit: false });
+        this.initData();
+    };
+
+    onCancel = () => {
+        this.setState({ isEdit: false });
+    };
+
+    render() {
+        const { info, isEdit } = this.state;
+
+        if (isEdit) return <EditForm info={info} onSave={this.onSave} onCancel={this.onCancel} />;
+
+        return (
+            <>
+                <Descriptions column={1} layout="vertical" colon={false}>
+                    <Item label="验证码长度">{OTP_LENGTH_TEXT[info.length]}</Item>
+                    <Item label="验证码类型">{OTP_TYPE_TEXT[info.type]}</Item>
+                    <Item label="验证码有效期">{OTP_EXPIRE_TEXT[info.expireMin]}</Item>
+                    <Item label="允许失败次数">{OTP_FAIL_TEXT[info.maxFailCount]}</Item>
+                    <Item label="验证码模板">{OTP_TEMPLATE_TEXT[info.template]}</Item>
+                </Descriptions>
+                <Button onClick={this.showEdit} style={{ marginTop: 10, marginBottom: 24 }}>
+                    编辑
+                </Button>
+            </>
+        );
+    }
+}
+
+export default connect(({ appManage }) => ({ appManage }))(OtpConfig);
