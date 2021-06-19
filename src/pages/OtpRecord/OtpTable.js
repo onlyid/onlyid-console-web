11691208b5e4066409a0a089da95f88fa1c03a52
ltb <liangtb@qq.com>
2021-06-19 19:05:12 +0800

    feat: 移除otp记录的发送内容

--- src/pages/OtpRecord/OtpTable.js
+++ src/pages/OtpRecord/OtpTable.js
@@ -22,12 +22,11 @@ export default function({ list, loading, current, pageSize, total, onPaginationC
                 <TableRow>
                     <TableCell>接收人</TableCell>
                     <TableCell>验证码</TableCell>
-                    <TableCell>发送内容</TableCell>
                     <TableCell>创建时间</TableCell>
                     <TableCell>过期时间</TableCell>
                     <TableCell>发送状态</TableCell>
-                    <TableCell>校验失败次数</TableCell>
-                    <TableCell>最多失败次数</TableCell>
+                    <TableCell style={{ width: 80 }}>校验失败次数</TableCell>
+                    <TableCell style={{ width: 80 }}>最多失败次数</TableCell>
                     <TableCell>校验状态</TableCell>
                 </TableRow>
             </TableHead>
@@ -36,7 +35,6 @@ export default function({ list, loading, current, pageSize, total, onPaginationC
                     <TableRow key={item.id} hover>
                         <TableCell>{item.recipient}</TableCell>
                         <TableCell>{item.code}</TableCell>
-                        <TableCell className={styles.otpContent}>{item.content}</TableCell>
                         <TableCell>{moment(item.createDate).format(DATE_TIME_FORMAT)}</TableCell>
                         <TableCell>{moment(item.expireDate).format(DATE_TIME_FORMAT)}</TableCell>
                         <TableCell>
